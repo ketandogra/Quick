99384f1483672de13c2a90c831c617d4a53d36b5
ketan dogra <ketandogra1367@gmail.com>
2023-08-17 23:17:12 +0530

    updated routes

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -10,7 +10,7 @@ const Routers = () => {
     <Routes>
     
         
-        <Route path="/" element={<ProtectedRouters/>}>
+        <Route path="/*" element={<ProtectedRouters/>}>
         <Route index element={<Home/>}/>
         </Route>
         <Route path='/login' element={<Login/>}/>
